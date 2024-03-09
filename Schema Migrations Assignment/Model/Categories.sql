23dfe996ccdcb7541aba5a87e2c563e8b5a3220b
JesperHLund <jesperlund9@hotmail.com>
2024-03-09 13:11:17 +0100

    Update Categories.sql
    Fix: Split alter table, as it didn't allow to add column AND add foreign key reference in one go

--- Schema Migrations Assignment/Model/Categories.sql
+++ Schema Migrations Assignment/Model/Categories.sql
@@ -1,11 +1,13 @@
-﻿--Create categories table 03/03/2024 v1.0
+--Create categories table 03/03/2024 v1.0
 CREATE TABLE categories (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(255) NOT NULL,
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
 );
 
 --Migration 1 of Products table
 --03/03/2024 V2 
 ALTER TABLE products
-ADD category_id INT,
+ADD category_id INT;
+
+ALTER TABLE products
 ADD FOREIGN KEY (category_id) REFERENCES categories(id);
