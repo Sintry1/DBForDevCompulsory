5afd068be45208d0fdb110b47b3fe1c343947c34
Sintry1 <Dominic_smith2012@hotmail.co.uk>
2024-03-10 16:55:39 +0100

    Fixed issue in Products file after merge

--- DBMigrations/Models/Products.cs
+++ DBMigrations/Models/Products.cs
@@ -10,14 +10,11 @@ namespace DBMigrations.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
-<<<<<<< HEAD
 
         [ForeignKey("ProductRatings")]
         public int Rating_Id { get; set; }
         public ProductRatings ProductRatings { get; set; }
-=======
         public int CategoryId { get; set; }
         public Categories Category { get; set; }
->>>>>>> ef/add-categories
     }
 }
