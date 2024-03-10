e6b19f8e577e2aa354cd3c3a504474e3daa231b0
Sintry1 <Dominic_smith2012@hotmail.co.uk>
2024-03-10 16:18:50 +0100

    Altered connection string variable and added a DB name variable

--- DBMigrations/DesignTimeDbContextFactory.cs
+++ DBMigrations/DesignTimeDbContextFactory.cs
@@ -6,11 +6,15 @@ namespace DBMigrations
 {
     public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<DBContext>
     {
+        // Change this to the name of your local MSSQL server.
         private string ConnectionString = "DESKTOP-VAUPBM7";
+
+        // Change this to whatever you want the name of your database to be.
+        private string DatabaseName = "EFCoreMigrations";
         public DBContext CreateDbContext(string[] args)
         {
             var optionsBuilder = new DbContextOptionsBuilder<DBContext>();
-            optionsBuilder.UseSqlServer($"Server={ConnectionString};Database=EFCoreMigrations;Trusted_Connection=True;TrustServerCertificate=true");
+            optionsBuilder.UseSqlServer($"Server={ConnectionString};Database={DatabaseName};Trusted_Connection=True;TrustServerCertificate=true");
 
             return new DBContext(optionsBuilder.Options);
         }
